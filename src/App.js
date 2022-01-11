bb3d42c01acf634a92728844342dc636d6553683
Magnus Karström <magnus@yesweb.se>
2022-01-11 15:27:56 +0100

    Working on global styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const App = () => {
-  return <div classname="App">Start here.</div>;
-};
+function App() {
+  return (
+    <div classname="App">
+      Start here.
+    </div>
+  );
+}
 
 export default App;
