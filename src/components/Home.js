a515d1e23f5d0cb1735a51d92ed21e4bb918e53b
Magnus Karström <magnus@yesweb.se>
2022-01-13 15:29:24 +0100

    At 1:46:45 and it has fetched the data!

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,24 +20,22 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-const fetchMovies = async (page, searchTerm) ="") => {
-  try {
-    setError(false);
-    setLoading(true);
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false);
+      setLoading(true);
 
-    const movies = await API.fetchMovies(searchTerm, page);
-    console.log(movies);
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+    } catch (error) {
+      setError(true);
+    }
+  };
 
-  } catch (error) {
-    setError(true);
-  }
-
-}
-
-//Initial render
-useEffect(() =>{
-fetchMovies(1)
-},[])
+  //Initial render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
 
   return <div>Home Page</div>;
 };
